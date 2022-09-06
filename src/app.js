bde9ca1989a68c166b60a63ce1338e3bf048ccdc
refszin <refwire@gmail.com>
2022-09-05 22:47:59 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express, {json} from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import cors from "cors";
+import chalk from "chalk";
+import dotenv from "dotenv";
+import dayjs from "dayjs";
+import joi from "joi";
+
+const app = express();
+app.use(cors());
+app.use(json());
+dotenv.config();
+
+const PORT = process.env.PORTA;
+const NOME = process.env.NOMEE;
+let db ;
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+const promise = mongoClient.connect();
+
+promise.then(() => 
+  db = mongoClient.db('UOL__DB')
+);
+promise.catch(err =>
+  console.log("Não foi possível se conectar", err)
+);
+
+
+app.listen(PORT, () => { console.log(chalk.green.bold(`Rodando ${NOME} Lisu na Porta: ${PORT}`))});
